89d60dfa1dfefa9a5ceaf9587182b1adc89f72cf
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-26 17:19:16 +0300

    Ouput posts from posts.json

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup>
+    import { ref } from 'vue'
     import SearchPanel from './components/SearchPanel/index.vue'
+    import Post from './components/Post/index.vue'
+
+    const posts = ref([])
+
+    const request = fetch('server/posts.json').then(response => {
+        return response.json()
+    })
+
+    request.then(data => posts.value = data)
 </script>
 
 <template>
     <div class="content">
         <SearchPanel />
-        <section class="posts"></section>
+        <section class="posts">
+            <Post
+                v-for="post in posts"
+                :name="post.name"
+                :desc="post.desc"
+            />
+        </section>
     </div>
     <div class="tags"></div>
 </template>
@@ -14,6 +30,9 @@
     .posts {
         border: 1px solid #4f2b9d;
         padding: 10px;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 10px;
     }
 
     .tags {
---
+++ src/components/Post/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+    const props = defineProps({
+        name: String,
+        desc: String
+    })
+</script>
+
+<template>
+    <article class="post">
+        <h2>{{ name }}</h2>
+        <p>{{ desc }}</p>
+    </article>
+</template>
+
+<style scoped lang="scss">
+    .post {
+        border-radius: 5px;
+        border: 1px solid #000;
+        padding: 10px;
+        height: 160px;
+        overflow: hidden;
+        white-space: normal;
+        text-overflow: ellipsis;
+    }
+</style>
