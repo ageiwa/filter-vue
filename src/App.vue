673a82a9c89989142ced481768576b5e29c4e28c
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-26 16:48:23 +0300

    Basic template markup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
-
+    import SearchPanel from './components/SearchPanel/index.vue'
 </script>
 
 <template>
-
+    <div class="content">
+        <SearchPanel />
+        <section class="posts"></section>
+    </div>
+    <div class="tags"></div>
 </template>
 
 <style scoped>
+    .posts {
+        border: 1px solid #4f2b9d;
+        padding: 10px;
+    }
 
+    .tags {
+        border: 1px solid #9d2b2b;
+    }
 </style>
---
+++ src/components/SearchPanel/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="search-panel">
+        <input class="search-panel__input" type="text" placeholder="Начните вводить название">
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .search-panel {
+        padding: 10px;
+        border: 1px solid #000;
+
+        .search-panel__input {
+            padding: 5px;
+            width: 100%;
+        }
+    }
+</style>
