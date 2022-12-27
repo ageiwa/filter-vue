6dbe627ec520e6166fb0d64e4bab28ac04600ab1
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-27 09:59:35 +0300

    When you click on the button, the closing icon appears

--- src/components/Tag/index.vue
+++ src/components/Tag/index.vue
@@ -13,8 +13,11 @@
 </script>
 
 <template>
-    <div :class="active" @click="toggle" class="tag">
-        <p>{{ name }}</p>
+    <div :class="['tag', active]" @click="toggle">
+        <p class="tag__title">{{ name }}</p>
+        <div v-if="active" class="tag__btn">
+            <img class="btn__icon" src="./cancel.svg" alt="close">
+        </div>
     </div>
 </template>
 
@@ -23,12 +26,30 @@
         margin: 5px;
         border: 1px solid #000;
         padding: 10px;
+        display: flex;
         user-select: none;
         cursor: pointer;
+
+        .tag__title {
+            white-space: nowrap;
+        }
     }
 
     .tag_active {
         background-color: #000;
         color: #fff;
+    }
+
+    .tag__btn {
+        width: 18px;
+        height: 18px;
+        margin-left: 5px;
+        border-radius: 50%;
+        background-color: #fff;
+
+        .btn__icon {
+            width: 100%;
+            height: 100%;
+        }
     }
 </style>
